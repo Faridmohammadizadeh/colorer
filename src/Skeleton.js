f3eda50cf7f19dfbfcb2c1c7fcfe4640c03b27de
Farid <mohammadizadeh79@gmail.com>
2023-10-10 14:47:33 +0330

    skatch page completed

--- src/Skeleton.js
+++ src/Skeleton.js
@@ -29,12 +29,13 @@ function Skeleton() {
   // ---------------------------------- screen size
 
   // color picker settings ---------------------------------------------
-  const [backgroundColor, setBackgroundColor] = useState("#e7e7e7");
-  const [textColor, setTextColor] = useState("#4b4b4b");
-  const [buttonColor, setButtonColor] = useState("#70efff");
-  const [deatailColor, setDeatailColor] = useState("#3f9681");
+  const [backgroundColor, setBackgroundColor] = useState("#ADC4CE");
+  const [textColor, setTextColor] = useState("#F1F0E8");
+  const [buttonColor, setButtonColor] = useState("#EEE0C9");
+  const [deatailColor, setDeatailColor] = useState("#96B6C5");
 
-  // color picker settings ---------------------------------------------
+  // nav baneer  components ---------------------------------------------
+
   function NavItem() {
     return (
       <div
@@ -51,7 +52,25 @@ function Skeleton() {
       ></div>
     );
   }
-  //  --------------------------------------------- color picker settings
+  //cloud button component  ---------------------------------------------
+
+  const clicked = useCallback(({ target: { innerText } }) => {
+    console.log(`Clicked on "${innerText}"!`);
+  }, []);
+
+  function SaveBtn() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="30"
+        height="30"
+        class="icon"
+      >
+        <path d="M22,15.04C22,17.23 20.24,19 18.07,19H5.93C3.76,19 2,17.23 2,15.04C2,13.07 3.43,11.44 5.31,11.14C5.28,11 5.27,10.86 5.27,10.71C5.27,9.33 6.38,8.2 7.76,8.2C8.37,8.2 8.94,8.43 9.37,8.8C10.14,7.05 11.13,5.44 13.91,5.44C17.28,5.44 18.87,8.06 18.87,10.83C18.87,10.94 18.87,11.06 18.86,11.17C20.65,11.54 22,13.13 22,15.04Z"></path>
+      </svg>
+    );
+  }
 
   return (
     <>
@@ -181,7 +200,28 @@ function Skeleton() {
 
         <div className="control-box">
           <div className="control-section">
-            <h1 style={{ color: backgroundColor }}>{backgroundColor}</h1>
+            <div className="control-hex-buttons-container">
+              <input
+                type="text"
+                autocomplete="off"
+                name="backgroundField"
+                class="input"
+                value={"#" + backgroundColor.slice(1)}
+                maxLength="7"
+                minLength="7"
+                onChange={(e) => setBackgroundColor(e.target.value)}
+                style={{
+                  "box-shadow": `inset 2px 5px 10px ${backgroundColor}`,
+                }}
+              />
+
+              <CopyToClipboard text={backgroundColor}>
+                <button class="btnCloud" style={{ fill: backgroundColor }}>
+                  <SaveBtn />
+                </button>
+              </CopyToClipboard>
+            </div>
+
             <HexColorPicker
               color={backgroundColor}
               onChange={(val) => {
@@ -191,7 +231,28 @@ function Skeleton() {
           </div>
 
           <div className="control-section">
-            <h1 style={{ color: deatailColor }}>{deatailColor}</h1>
+            <div className="control-hex-buttons-container">
+              <input
+                type="text"
+                autocomplete="off"
+                name="detailField"
+                class="input"
+                value={"#" + deatailColor.slice(1)}
+                maxLength="7"
+                minLength="7"
+                onChange={(e) => setDeatailColor(e.target.value)}
+                style={{
+                  "box-shadow": `inset 2px 5px 10px ${deatailColor}`,
+                }}
+              />
+
+              <CopyToClipboard text={deatailColor}>
+                <button class="btnCloud" style={{ fill: deatailColor }}>
+                  <SaveBtn />
+                </button>
+              </CopyToClipboard>
+            </div>
+
             <HexColorPicker
               color={deatailColor}
               onChange={(val) => {
@@ -201,7 +262,28 @@ function Skeleton() {
           </div>
 
           <div className="control-section">
-            <h1 style={{ color: textColor }}>{textColor}</h1>
+            <div className="control-hex-buttons-container">
+              <input
+                type="text"
+                autocomplete="off"
+                name="textField"
+                class="input"
+                value={"#" + textColor.slice(1)}
+                maxLength="7"
+                minLength="7"
+                onChange={(e) => setTextColor(e.target.value)}
+                style={{
+                  "box-shadow": `inset 2px 5px 10px ${textColor}`,
+                }}
+              />
+
+              <CopyToClipboard text={textColor}>
+                <button class="btnCloud" style={{ fill: textColor }}>
+                  <SaveBtn />
+                </button>
+              </CopyToClipboard>
+            </div>
+
             <HexColorPicker
               color={textColor}
               onChange={(val) => {
@@ -211,7 +293,28 @@ function Skeleton() {
           </div>
 
           <div className="control-section">
-            <h1 style={{ color: buttonColor }}>{buttonColor}</h1>
+            <div className="control-hex-buttons-container">
+              <input
+                type="text"
+                autocomplete="off"
+                name="buttonField"
+                class="input"
+                value={"#" + buttonColor.slice(1)}
+                maxLength="7"
+                minLength="7"
+                onChange={(e) => setButtonColor(e.target.value)}
+                style={{
+                  "box-shadow": `inset 2px 5px 10px ${buttonColor}`,
+                }}
+              />
+
+              <CopyToClipboard text={buttonColor}>
+                <button class="btnCloud" style={{ fill: buttonColor }}>
+                  <SaveBtn />
+                </button>
+              </CopyToClipboard>
+            </div>
+
             <HexColorPicker
               color={buttonColor}
               onChange={(val) => {
